74c5a72be3886ac45412fe50df54baebb86b0b07
Levi Sanchez <tkd.lsanchez@gmail.com>
2019-12-12 19:36:10 -0700

    Followed along with Brad from his YouTube channel Traversy Media in a Vue JS crash course.

---
+++ vue-js-crash-course-2019/README.md
@@ -0,0 +1,4 @@
+# Vue JS Crash Course -2019
+
+* By: Traversy Media
+* From: < https://www.youtube.com/watch?v=Wy9q22isx3U >
---
+++ vue-js-crash-course-2019/vue_crash_todolist/src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It is part of the Vue crash course on Youtube</p>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="css" scoped>
+</style>
