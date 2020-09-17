b7dcee1810532eebc2d778015715196b147a62ab
Levi Sanchez <tkd.lsanchez@gmail.com>
2020-09-17 17:35:59 -0600

    Setting up basic boiler plate stuff for the project

---
+++ vue-js-and-firestore-app/employeemanager/src/components/Navbar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div id="navbar">
+        <h3>Navbar</h3>
+    </div>
+</template>
+
---
+++ vue-js-and-firestore-app/employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAjId-ueLEN0zMxBo8AHaZxU1Y70slvk3s",
+    authDomain: "play-ground-924cd.firebaseapp.com",
+    databaseURL: "https://play-ground-924cd.firebaseio.com",
+    projectId: "play-ground-924cd",
+    storageBucket: "play-ground-924cd.appspot.com",
+    messagingSenderId: "581672529289",
+    appId: "1:581672529289:web:4c71c5ffc8e4bc24b54558"
+}
